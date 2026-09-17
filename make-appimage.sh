2d6f5c46a778ccf30632ac44d1620a02eed8dcb6
Nemo <328747105+Nemo-010@users.noreply.github.com>
2026-09-17 17:43:50 +0000

    use host GPU drivers
    Ship zero GPU drivers and load the host ones through cross-libc-dlopen;
    USE_HOST_DRIVERS_EXPERIMENTAL also filters driver libs out of the tracing
    pass and keeps the anylinux GL forwards.

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
